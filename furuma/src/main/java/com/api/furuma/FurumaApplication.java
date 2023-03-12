bf8860c46eaf8e910a9904c9e2404fb5d1bc5218
Huu Le Long Nguyen <long.nguyen211996@gmail.com>
2023-03-12 08:42:22 +0700

    update

---
+++ furuma/src/main/java/com/api/furuma/FurumaApplication.java
@@ -0,0 +1,13 @@
+package com.api.furuma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurumaApplication.class, args);
+	}
+
+}
